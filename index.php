de118ddd69963ef4fd7e97880a5dead0b8cc15e3
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-19 18:22:42 +0100

    aggiornato if e rimosso $_ prima del get che dava warning + aggiunto name ai tre input

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // apri session
 session_start();
 
-if ($server['REQUEST_METHOD'] === '$_GET' && isset($_GET['password_length'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['password_length'])) {
     // recuper la password
     $password_length = $_GET['password_length'];
 
@@ -51,7 +51,8 @@ function createRandomPassword($length, $includeLetters, $includeNumbers, $includ
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Strong Password Generator</title>
     <!-- bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- css -->
     <link rel="stylesheet" href="./css/style.css">
 
@@ -75,19 +76,19 @@ function createRandomPassword($length, $includeLetters, $includeNumbers, $includ
 
                     <!-- inclusione lettere -->
                     <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="letters">
+                        <input type="checkbox" class="form-check-input" id="letters" name="letters">
                         <label class="form-check-label" for="letters">Letters</label>
                     </div>
 
                     <!-- inclusione numeri -->
                     <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="numbers">
+                        <input type="checkbox" class="form-check-input" id="numbers" name="numbers">
                         <label class="form-check-label" for="numbers">Numbers</label>
                     </div>
 
                     <!-- inclusione simboli -->
                     <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="symbols">
+                        <input type="checkbox" class="form-check-input" id="symbols" name="symbols">
                         <label class="form-check-label" for="symbols">Symbols</label>
                     </div>
 
@@ -95,13 +96,15 @@ function createRandomPassword($length, $includeLetters, $includeNumbers, $includ
                     <div class="row mb-3">
                         <p>Allow repetition of characters: </p>
                         <div class="form-check ms-2">
-                            <input class="form-check-input" type="radio" name="character-repetition-yes" id="character-repetition-yes">
+                            <input class="form-check-input" type="radio" name="character-repetition-yes"
+                                id="character-repetition-yes">
                             <label class="form-check-label" for="character-repetition-yes">
                                 Yes
                             </label>
                         </div>
                         <div class="form-check ms-2">
-                            <input class="form-check-input" type="radio" name="character-repetition-no" id="character-repetition-no">
+                            <input class="form-check-input" type="radio" name="character-repetition-no"
+                                id="character-repetition-no">
                             <label class="form-check-label" for="character-repetition-no">
                                 No
                             </label>
@@ -111,7 +114,9 @@ function createRandomPassword($length, $includeLetters, $includeNumbers, $includ
                 </form>
                 <!-- Div con la nuova password -->
                 <div class="col-10 offset-1 mt-3 bg-light">
-                    <p>New Password: <?php echo isset($cryptedPassword) ? $cryptedPassword : ''; ?></p>
+                    <p>New
+                        Password:<?php echo isset($_SESSION['cryptedPassword']) ? $_SESSION['cryptedPassword'] : ''; ?>
+                    </p>
                 </div>
             </div>
         </section>
